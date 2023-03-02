b1d8150f5ffa427f02431e0e57dcd60dd834c1ad
Jade1724 <haruka.i1724@gmail.com>
2023-03-02 14:03:57 +1300

    Showing the list of products on HomePage.

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,11 @@
 import Head from "next/head";
 import Title from "@/components/Title";
 
+const products = [
+  { id: 1, title: "First product" },
+  { id: 2, title: "Second product" },
+];
+
 export default function HomePage() {
   return (
     <>
@@ -9,7 +14,11 @@ export default function HomePage() {
       </Head>
       <main className="p-4">
         <Title>Next Shop</Title>
-        <p>[TODO: display products]</p>
+        <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul>
       </main>
     </>
   );
