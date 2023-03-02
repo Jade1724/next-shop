70b1d8648c61ea7dbdbb50f1098ec1066c16d767
Jade1724 <haruka.i1724@gmail.com>
2023-03-02 14:04:15 +1300

    Sever side products data fetching.

---
+++ pages/index-1.js
@@ -0,0 +1,30 @@
+// Options 1: Fetch products on the server side (in getStaticProps)
+import { useEffect } from "react";
+import Head from "next/head";
+import Title from "@/components/Title";
+import { getProducts } from "@/lib/product";
+
+export async function getStaticProps() {
+  console.log("[HomePage] getStaticProps()");
+  const products = await getProducts();
+  return { props: { products } };
+}
+export default function HomePage(context) {
+  console.log(context);
+  useEffect(() => {});
+  return (
+    <>
+      <Head>
+        <title>Next Shop</title>
+      </Head>
+      <main className="p-4">
+        <Title>Next Shop</Title>
+        {/* <ul>
+          {products.map((product) => (
+            <li key={product.id}>{product.title}</li>
+          ))}
+        </ul> */}
+      </main>
+    </>
+  );
+}
